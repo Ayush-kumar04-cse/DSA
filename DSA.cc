f84faa8527bf46f639a4ee05e42f9ce2436cf587
Ayush kumar <ankitsabi04@gmail.com>
2026-08-19 18:48:01 +0530

    Add my name

--- DSA.cc
+++ DSA.cc
@@ -1,56 +1,56 @@
-// # include <stdio.h>
+# include <stdio.h>
 
-// int main()
-// {
-//     int arr[5];
-//     for (int i = 0;i<5;i++)
-//     {
-//         scanf("%d",&arr[i]);
-//     }
-//     printf("\n");
-//     for (int i = 0; i<5;i++)
-//     {
-//         printf("%d",arr[i]);
-//         printf("\n");
-//     }
-// }
+int main()
+{
+    int arr[5];
+    for (int i = 0;i<5;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("\n");
+    for (int i = 0; i<5;i++)
+    {
+        printf("%d",arr[i]);
+        printf("\n");
+    }
+}
 
-// # include <stdio.h>
+# include <stdio.h>
 
-// int main(){
-//     int arr[10];
-//     int n = 5,a,b;
-//     printf("Enter your number : ");
-//     scanf("%d",&a);
-//     printf("Enter the index number : ");
-//     scanf("%d",&b);
+int main(){
+    int arr[10];
+    int n = 5,a,b;
+    printf("Enter your number : ");
+    scanf("%d",&a);
+    printf("Enter the index number : ");
+    scanf("%d",&b);
 
-//     for(int i = 0;i <n;i++)
-//     {
-//          printf("Enter the %d array element: ",i);
-//         scanf("%d",&arr[i]);
-//     }
-//     printf("my array: ");
+    for(int i = 0;i <n;i++)
+    {
+         printf("Enter the %d array element: ",i);
+        scanf("%d",&arr[i]);
+    }
+    printf("my array: ");
 
-//     for(int i = 0;i <n;i++)
-//     {
-//         printf(" %d ",arr[i]);
-//     }
-//     int i = n-1;
-//     for(;i>=b;i--)
-//     {
-//         arr[i+1] = arr[i];
-//     }
-//     arr[i+1] = a;
-//     n++;
-//     printf("\n");
-//     printf("my array after inserting %d at index %d: ",a,b);
-//     for(int i = 0;i <n;i++)
-//     {
-//         printf(" %d ",arr[i]);
-//     }
+    for(int i = 0;i <n;i++)
+    {
+        printf(" %d ",arr[i]);
+    }
+    int i = n-1;
+    for(;i>=b;i--)
+    {
+        arr[i+1] = arr[i];
+    }
+    arr[i+1] = a;
+    n++;
+    printf("\n");
+    printf("my array after inserting %d at index %d: ",a,b);
+    for(int i = 0;i <n;i++)
+    {
+        printf(" %d ",arr[i]);
+    }
 
-// }
+}
 
 # include <stdio.h>
 int main()
@@ -76,4 +76,11 @@ int main()
     }
     
 
+}
+
+# include <stdio.h>
+int main()
+{
+    printf("Ayush kuamr ");
+    return 0;
 }
