4b909fc954ed4b1a0f7edd52b20a7605bf43d170
Ayush kumar <ankitsabi04@gmail.com>
2026-08-28 14:59:43 +0530

    selection sort

---
+++ LPI_28_8_.cpp
@@ -0,0 +1,78 @@
+/*  fint the smallest element index */
+// # include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[10] = {12,2,45,5566,23};
+//     int n = 5;
+
+//     int flag = 0;
+//     for (int i = 1 ;i <n;i++)
+//     {
+//         if (arr[i] < arr[flag])
+//         {   
+//             flag = i;
+
+//         } 
+//     }
+//     cout<<flag;
+    
+//     return 0;
+// }
+
+// # include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[10] = {12,2,45,5566,23};
+//     int n = 5;
+
+//     int flag = 0;
+//     for (int i = 1 ;i <n;i++)
+//     {
+//         if (arr[i] < arr[flag])
+//         {   
+//             flag = i;
+
+//         } 
+//     }
+//     swap(arr[0], arr[flag]);
+//     for (int i = 0;i<n;i++)
+//     {
+//         cout<<arr[i];
+//     }
+    
+//     return 0;
+// }
+
+
+# include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5] = {12,2,45,5566,23};
+    int n = 5;
+
+    for(int i = 0; i<n-1;i++)
+    {
+        int m = i;
+        for (int j = i+1 ;j <n;j++)
+        {
+            if (arr[j] < arr[m])
+            {   
+                m = j;
+
+            } 
+        }
+        swap(arr[i],arr[m]);
+
+    
+    }
+    for(int i = 0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
+
