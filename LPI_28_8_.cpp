a9856d75ff9ee6f2ccf32d3eeb7eaf830c647042
Ayush kumar <ankitsabi04@gmail.com>
2026-09-14 13:38:18 +0530

    Nothing more

--- LPI_28_8_.cpp
+++ LPI_28_8_.cpp
@@ -18,40 +18,40 @@ int main()
     swap(arr[0], arr[flag]);
     for (int i = 0;i<n;i++)
     {
-        cout<<arr[i];
-    }
-    
-    return 0;
-}
-
-
-# include <iostream>
-using namespace std;
-int main()
-{
-    int arr[5] = {12,2,45,5566,23};
-    int n = 5;
-
-    for(int i = 0; i<n-1;i++)
-    {
-        int m = i;
-        for (int j = i+1 ;j <n;j++)
-        {
-            if (arr[j] < arr[m])
-            {   
-                m = j;
-
-            } 
-        }
-        swap(arr[i],arr[m]);
-
-    
-    }
-    for(int i = 0;i<n;i++)
-    {
         cout<<arr[i]<<" ";
     }
-    cout<<endl;
+    
     return 0;
 }
+
+
+// # include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[5] = {12,2,45,5566,23};
+//     int n = 5;
+
+//     for(int i = 0; i<n-1;i++)
+//     {
+//         int m = i;
+//         for (int j = i+1 ;j <n;j++)
+//         {
+//             if (arr[j] < arr[m])
+//             {   
+//                 m = j;
+
+//             } 
+//         }
+//         swap(arr[i],arr[m]);
+
+    
+//     }
+//     for(int i = 0;i<n;i++)
+//     {
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+//     return 0;
+// }
 
